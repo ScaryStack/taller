afd013e4e904d59c4097f480933422078a9054a1
Patricio <patriciojimenez838@gmail.com>
2025-05-26 11:07:21 -0400

    Agrega relación a tabla Auth, Vehiculo y Notificacion

--- cliente/Cliente/src/main/java/com/microservicios_taller/Cliente/Cliente/model/Cliente.java
+++ cliente/Cliente/src/main/java/com/microservicios_taller/Cliente/Cliente/model/Cliente.java
@@ -1,5 +1,6 @@
 package com.microservicios_taller.Cliente.Cliente.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -31,6 +32,18 @@ public class Cliente {
 
     @Column(nullable = false)
     private String contrasena;
+
+    @OneToOne(mappedBy = "cliente", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private Auth auth;
+
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<Vehiculo> vehiculos;
+
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<Notificacion> notificaciones;
 
 
 }
