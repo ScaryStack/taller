8f750b88a7a5afc00111252aeda32a317cebf8a9
Patricio <patriciojimenez838@gmail.com>
2025-05-26 11:06:44 -0400

    Agrega relación a tabla cliente

--- auth/Auth/src/main/java/com/microservicios_taller/Auth/Auth/model/Auth.java
+++ auth/Auth/src/main/java/com/microservicios_taller/Auth/Auth/model/Auth.java
@@ -1,5 +1,6 @@
 package com.microservicios_taller.Auth.Auth.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,4 +17,9 @@ public class Auth {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idAuth;
 
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "idCliente")
+    @JsonBackReference
+    private Cliente cliente;
 }
