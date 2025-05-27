a6625e180983aab85ebf5c452187eb49d46724b2
Patricio <patriciojimenez838@gmail.com>
2025-05-27 05:42:20 -0400

    Modifica Cita con relacion de Servicio

--- cita/Cita/src/main/java/com/microservicios_taller/Cita/Cita/model/Cita.java
+++ cita/Cita/src/main/java/com/microservicios_taller/Cita/Cita/model/Cita.java
@@ -1,7 +1,5 @@
 package com.microservicios_taller.Cita.Cita.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,22 +13,12 @@ import lombok.NoArgsConstructor;
 public class Cita {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idCita;
+    private Integer idCita;
 
     @Column(nullable = false)
     private String mensajeCita;
 
-    @ManyToOne
-    @JoinColumn(name = "idServicio")
-    @JsonBackReference
-    private Servicio servicio;
-
-    @OneToOne(mappedBy = "cita", cascade = CascadeType.ALL)
-    @JsonManagedReference
-    private HistorialCitas historial;
-
-    @OneToOne(mappedBy = "cita", cascade = CascadeType.ALL)
-    @JsonManagedReference
-    private Factura factura;
+    @Column(nullable = false)
+    private Integer idServicio;
 
 }
